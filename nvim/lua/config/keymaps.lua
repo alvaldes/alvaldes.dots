cd5626408c2fb86ba75615e2aca289f14e711b4a
alvaldes <angelluis2605@gmail.com>
2025-03-16 22:24:42 -0600

    feat(keymaps): add new keymaps for select all and clipboard

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,44 +3,58 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local keymap = vim.keymap
 
--- Set the x delete key to not copy to the system clipboard
-vim.keymap.set("n", "x", '"_x', { noremap = true, silent = true })
+-- Do things without affecting the registers
+keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>P", '"0P')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>D", '"_D')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
 
 -- Position cursor at the middle of the screen after scrolling half page
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Scroll down half a page and center the cursor
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Scroll up half a page and center the cursor
+keymap.set("n", "<C-d>", "<C-d>zz") -- Scroll down half a page and center the cursor
+keymap.set("n", "<C-u>", "<C-u>zz") -- Scroll up half a page and center the cursor
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Position cursor at the middle of the screen after moving
--- vim.keymap.set("n", "j", "jzz") -- Move cursor down and center the screen
--- vim.keymap.set("n", "k", "kzz") -- Move cursor up and center the screen
+-- keymap.set("n", "j", "jzz") -- Move cursor down and center the screen
+-- keymap.set("n", "k", "kzz") -- Move cursor up and center the screen
 
 -- Position cursor at the middle of the screen after searching
--- vim.keymap.set("n", "n", "nzz") -- Keep cursor in the middle when searching forward
--- vim.keymap.set("n", "N", "Nzz") -- Keep cursor in the middle when searching backward
+-- keymap.set("n", "n", "nzz") -- Keep cursor in the middle when searching forward
+-- keymap.set("n", "N", "Nzz") -- Keep cursor in the middle when searching backward
 
 -- Map Ctrl+b in insert mode to delete to the end of the word without leaving insert mode
-vim.keymap.set("i", "<C-b>", "<C-o>de")
+keymap.set("i", "<C-b>", "<C-o>de")
 
 -- Map Ctrl+c to escape from other modes
-vim.keymap.set({ "i", "n", "v" }, "<C-c>", [[<C-\><C-n>]])
+keymap.set({ "i", "n", "v" }, "<C-c>", [[<C-\><C-n>]])
 
 -- Move lines up and down in visual mode
-vim.keymap.set("v", "J", ":move '>+1<CR>gv=gv", { desc = "Move line down in visual mode" })
-vim.keymap.set("v", "K", ":move '<-2<CR>gv=gv", { desc = "Move line up in visual mode" })
+keymap.set("v", "J", ":move '>+1<CR>gv=gv", { desc = "Move line down in visual mode" })
+keymap.set("v", "K", ":move '<-2<CR>gv=gv", { desc = "Move line up in visual mode" })
 
 -- When you do joins with J it will keep your cursor at the beginning instead of at the end
-vim.keymap.set("n", "J", "mzJ`z")
+keymap.set("n", "J", "mzJ`z")
 
 ----- Tmux Navigation ------
 local nvim_tmux_nav = require("nvim-tmux-navigation")
 -- Jump to previous definition and center the cursor
-vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft) -- Navigate to the left pane
-vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown) -- Navigate to the bottom pane
-vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp) -- Navigate to the top pane
-vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight) -- Navigate to the right pane
-vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive) -- Navigate to the last active pane
-vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext) -- Navigate to the next pane
+keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft) -- Navigate to the left pane
+keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown) -- Navigate to the bottom pane
+keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp) -- Navigate to the top pane
+keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight) -- Navigate to the right pane
+keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive) -- Navigate to the last active pane
+keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext) -- Navigate to the next pane
 
 -- REQUIRED
 local harpoon = require("harpoon")
@@ -48,35 +62,35 @@ harpoon:setup()
 -- REQUIRED
 
 -- Set keybindings for VimTeX (these are just examples)
-vim.keymap.set("n", "<leader>tl", "<cmd>VimtexCompile<CR>", { desc = "Start compiling LaTeX" })
-vim.keymap.set("n", "<leader>tv", "<cmd>VimtexView<CR>", { desc = "View compiled PDF" })
-vim.keymap.set("n", "<leader>tk", "<cmd>VimtexStop<CR>", { desc = "Stop LaTeX compilation" })
-vim.keymap.set("n", "<leader>q", ":!zathura <C-r ≥ expand ('%:r') <cr> - pdf &<cr>", { desc = "Open Zathura" })
+keymap.set("n", "<leader>tl", "<cmd>VimtexCompile<CR>", { desc = "Start compiling LaTeX" })
+keymap.set("n", "<leader>tv", "<cmd>VimtexView<CR>", { desc = "View compiled PDF" })
+keymap.set("n", "<leader>tk", "<cmd>VimtexStop<CR>", { desc = "Stop LaTeX compilation" })
+keymap.set("n", "<leader>q", ":!zathura <C-r ≥ expand ('%:r') <cr> - pdf &<cr>", { desc = "Open Zathura" })
 
 ----- OBSIDIAN -----
-vim.keymap.set(
+keymap.set(
   "n",
   "<leader>oc",
   "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>",
   { desc = "Obsidian Check Checkbox" }
 )
-vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
-vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open in Obsidian App" })
-vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
-vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
-vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
-vim.keymap.set("n", "<leader>odl", "<cmd>ObsidianDailies<CR>", { desc = "List Dailies" })
-vim.keymap.set("n", "<leader>odt", "<cmd>ObsidianToday<CR>", { desc = "Create/Open Today Daily Note" })
-vim.keymap.set("n", "<leader>odm", "<cmd>ObsidianTomorrow<CR>", { desc = "Create/Open Tomorrow Daily Note" })
-vim.keymap.set("n", "<leader>ody", "<cmd>ObsidianYesterday<CR>", { desc = "Create/Open Yesterday Daily Note" })
-vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
-vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
+keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
+keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open in Obsidian App" })
+keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
+keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
+keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
+keymap.set("n", "<leader>odl", "<cmd>ObsidianDailies<CR>", { desc = "List Dailies" })
+keymap.set("n", "<leader>odt", "<cmd>ObsidianToday<CR>", { desc = "Create/Open Today Daily Note" })
+keymap.set("n", "<leader>odm", "<cmd>ObsidianTomorrow<CR>", { desc = "Create/Open Tomorrow Daily Note" })
+keymap.set("n", "<leader>ody", "<cmd>ObsidianYesterday<CR>", { desc = "Create/Open Yesterday Daily Note" })
+keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
+keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
 
 -----  OIL -----
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Delete all buffers but the current one --
-vim.keymap.set(
+keymap.set(
   "n",
   "<leader>bq",
   '<Esc>:%bdelete|edit #|normal`"<Return>',
@@ -96,33 +110,33 @@ local function delete_mark(mark)
 end
 
 -- Keymap para eliminar una marca
-vim.keymap.set("n", "<leader>dm", function()
+keymap.set("n", "<leader>dm", function()
   local mark = vim.fn.input("Enter mark to delete: ")
   delete_mark(mark)
 end, { noremap = true, silent = true, desc = "Delete mark" })
 
 ----- HARPOON 2 -----
-vim.keymap.set("n", "<leader>M", function()
+keymap.set("n", "<leader>M", function()
   harpoon:list():add()
 end, { desc = "Add harpoon mark" })
 
-vim.keymap.set("n", "<C-e>", function()
+keymap.set("n", "<C-e>", function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 
-vim.keymap.set("n", "<C-H>", function()
+keymap.set("n", "<C-H>", function()
   harpoon:list():select(1)
 end)
 
-vim.keymap.set("n", "<C-J>", function()
+keymap.set("n", "<C-J>", function()
   harpoon:list():select(2)
 end)
 
-vim.keymap.set("n", "<C-K>", function()
+keymap.set("n", "<C-K>", function()
   harpoon:list():select(3)
 end)
 
-vim.keymap.set("n", "<C-L>", function()
+keymap.set("n", "<C-L>", function()
   harpoon:list():select(4)
 end)
 
@@ -141,7 +155,7 @@ vim.api.nvim_set_keymap("x", "J", "<Nop>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "K", "<Nop>", { noremap = true, silent = true })
 
 -- Open current file in finder
-vim.keymap.set("n", "<leader>fO", function()
+keymap.set("n", "<leader>fO", function()
   local file_path = vim.fn.expand("%:p")
   if file_path ~= "" then
     local command = "open -R " .. vim.fn.shellescape(file_path)
@@ -195,7 +209,7 @@ wk.add({
 })
 
 -- Open the current markdown file in the browser
-vim.keymap.set("n", "<leader>mo", function()
+keymap.set("n", "<leader>mo", function()
   local file = vim.fn.expand("%:p") -- Obtener ruta absoluta del archivo actual
 
   if not file:match("%.md$") then
@@ -211,7 +225,7 @@ end, { desc = "Open current Markdown file in browser", silent = true })
 -- Toggle bullet point at the beginning of the current line in normal mode
 -- If in a multiline paragraph, make sure the cursor is on the line at the top
 -- "d" is for "dash" lamw25wmal
--- vim.keymap.set("n", "<leader>md", function()
+-- keymap.set("n", "<leader>md", function()
 --   -- Get the current cursor position
 --   local cursor_pos = vim.api.nvim_win_get_cursor(0)
 --   local current_buffer = vim.api.nvim_get_current_buf()
@@ -261,25 +275,25 @@ end, { desc = "Open current Markdown file in browser", silent = true })
 -- Keymap to switch spelling language to English lamw25wmal
 -- To save the language settings configured on each buffer, you need to add
 -- "localoptions" to vim.opt.sessionoptions in the `lua/config/options.lua` file
-vim.keymap.set("n", "<leader>msle", function()
+keymap.set("n", "<leader>msle", function()
   vim.opt.spelllang = "en"
   vim.cmd("echo 'Spell language set to English'")
 end, { desc = "Spelling language English" })
 
 -- Keymap to switch spelling language to Spanish lamw25wmal
-vim.keymap.set("n", "<leader>msls", function()
+keymap.set("n", "<leader>msls", function()
   vim.opt.spelllang = "es"
   vim.cmd("echo 'Spell language set to Spanish'")
 end, { desc = "Spelling language Spanish" })
 
 -- Keymap to switch spelling language to both spanish and english lamw25wmal
-vim.keymap.set("n", "<leader>mslb", function()
+keymap.set("n", "<leader>mslb", function()
   vim.opt.spelllang = "en,es"
   vim.cmd("echo 'Spell language set to Spanish and English'")
 end, { desc = "Spelling language Spanish and English" })
 
 -- Show spelling suggestions / spell suggestions
-vim.keymap.set("n", "<leader>mss", function()
+keymap.set("n", "<leader>mss", function()
   -- Simulate pressing "z=" with "m" option using feedkeys
   -- vim.api.nvim_replace_termcodes ensures "z=" is correctly interpreted
   -- 'm' is the {mode}, which in this case is 'Remap keys'. This is default.
@@ -293,25 +307,25 @@ end, { desc = "Spelling suggestions" })
 
 -- markdown good, accept spell suggestion
 -- Add word under the cursor as a good word
-vim.keymap.set("n", "<leader>msg", function()
+keymap.set("n", "<leader>msg", function()
   vim.cmd("normal! zg")
 end, { desc = "Spelling add word to spellfile" })
 
 -- Undo zw, remove the word from the entry in 'spellfile'.
-vim.keymap.set("n", "<leader>msu", function()
+keymap.set("n", "<leader>msu", function()
   vim.cmd("normal! zug")
 end, { desc = "Spelling undo, remove word from list" })
 
 -- Repeat the replacement done by |z=| for all matches with the replaced word
 -- in the current window.
-vim.keymap.set("n", "<leader>msr", function()
+keymap.set("n", "<leader>msr", function()
   -- vim.cmd(":spellr")
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":spellr\n", true, false, true), "m", true)
 end, { desc = "Spelling repeat" })
 
 -- In visual mode, check if the selected text is already bold and show a message if it is
 -- If not, surround it with double asterisks for bold
-vim.keymap.set("v", "<leader>mb", function()
+keymap.set("v", "<leader>mb", function()
   -- Get the selected text range
   local start_row, start_col = unpack(vim.fn.getpos("'<"), 2, 3)
   local end_row, end_col = unpack(vim.fn.getpos("'>"), 2, 3)
@@ -330,7 +344,7 @@ end, { desc = "BOLD current selection" })
 -- -- If already bold, it will unbold the word under the cursor
 -- -- If you're in a multiline bold, it will unbold it only if you're on the
 -- -- first line
-vim.keymap.set("n", "<leader>mb", function()
+keymap.set("n", "<leader>mb", function()
   local cursor_pos = vim.api.nvim_win_get_cursor(0)
   local current_buffer = vim.api.nvim_get_current_buf()
   local start_row = cursor_pos[1] - 1
@@ -393,7 +407,7 @@ end, { desc = "BOLD toggle bold markers" })
 
 -- Paste a github link and add it in this format
 -- [folke/noice.nvim](https://github.com/folke/noice.nvim){:target="\_blank"}
-vim.keymap.set("n", "<leader>mg", function()
+keymap.set("n", "<leader>mg", function()
   -- Insert the text in the desired format
   vim.cmd('normal! a[](){:target="_blank"} ')
   vim.cmd("normal! F(pv2F/lyF[p")
@@ -407,7 +421,7 @@ end, { desc = "Paste Github link" })
 
 -- Use <CR> to fold when in normal mode
 -- To see help about folds use `:help fold`
-vim.keymap.set("n", "<CR>", function()
+keymap.set("n", "<CR>", function()
   -- Get the current line number
   local line = vim.fn.line(".")
   -- Get the fold level of the current line
@@ -470,14 +484,14 @@ local function fold_markdown_headings(levels)
 end
 
 -- Keymap for unfolding markdown headings of level 2 or above
-vim.keymap.set("n", "<leader>mfu", function()
+keymap.set("n", "<leader>mfu", function()
   -- Reloads the file to reflect the changes
   vim.cmd("edit!")
   vim.cmd("normal! zR") -- Unfold all headings
 end, { desc = "Unfold all headings level 2 or above" })
 
 -- Keymap for folding markdown headings of level 1 or above
-vim.keymap.set("n", "<leader>mfj", function()
+keymap.set("n", "<leader>mfj", function()
   -- Reloads the file to refresh folds, otherwise you have to re-open neovim
   vim.cmd("edit!")
   -- Unfold everything first or I had issues
@@ -487,7 +501,7 @@ end, { desc = "Fold all headings level 1 or above" })
 
 -- Keymap for folding markdown headings of level 2 or above
 -- I know, it reads like "madafaka" but "k" for me means "2"
-vim.keymap.set("n", "<leader>mfk", function()
+keymap.set("n", "<leader>mfk", function()
   -- Reloads the file to refresh folds, otherwise you have to re-open neovim
   vim.cmd("edit!")
   -- Unfold everything first or I had issues
@@ -496,7 +510,7 @@ vim.keymap.set("n", "<leader>mfk", function()
 end, { desc = "Fold all headings level 2 or above" })
 
 -- Keymap for folding markdown headings of level 3 or above
-vim.keymap.set("n", "<leader>mfl", function()
+keymap.set("n", "<leader>mfl", function()
   -- Reloads the file to refresh folds, otherwise you have to re-open neovim
   vim.cmd("edit!")
   -- Unfold everything first or I had issues
@@ -505,7 +519,7 @@ vim.keymap.set("n", "<leader>mfl", function()
 end, { desc = "Fold all headings level 3 or above" })
 
 -- Keymap for folding markdown headings of level 4 or above
-vim.keymap.set("n", "<leader>mf;", function()
+keymap.set("n", "<leader>mf;", function()
   -- Reloads the file to refresh folds, otherwise you have to re-open neovim
   vim.cmd("edit!")
   -- Unfold everything first or I had issues
@@ -521,7 +535,7 @@ end, { desc = "Fold all headings level 4 or above" })
 -- To generate the TOC I use the markdown-toc plugin
 -- https://github.com/jonschlinkert/markdown-toc
 -- And the markdown-toc plugin installed as a LazyExtra
-vim.keymap.set("n", "<leader>mt", function()
+keymap.set("n", "<leader>mt", function()
   local path = vim.fn.expand("%") -- Expands the current file name to a full path
   local bufnr = 0 -- The current buffer number, 0 references the current active buffer
   -- Save the current view
@@ -590,7 +604,7 @@ end, { desc = "Insert/update Markdown TOC" })
 _G.saved_positions = {}
 
 -- Mapping to jump to the first line of the TOC
-vim.keymap.set("n", "<leader>mm", function()
+keymap.set("n", "<leader>mm", function()
   -- Save the current cursor position
   _G.saved_positions["toc_return"] = vim.api.nvim_win_get_cursor(0)
   -- Perform a silent search for the <!-- toc --> marker and move the cursor two lines below it
@@ -607,7 +621,7 @@ vim.keymap.set("n", "<leader>mm", function()
 end, { desc = "Jump to the first line of the TOC" })
 
 -- Mapping to return to the previously saved cursor position
-vim.keymap.set("n", "<leader>mn", function()
+keymap.set("n", "<leader>mn", function()
   local pos = _G.saved_positions["toc_return"]
   if pos then
     vim.api.nvim_win_set_cursor(0, pos)
@@ -618,7 +632,7 @@ end, { desc = "Return to position before jumping" })
 -- Make sure to follow proper markdown convention, and you have a single H1
 -- heading at the very top of the file
 -- This will only search for H2 headings and above
-vim.keymap.set({ "n", "v" }, "gk", function()
+keymap.set({ "n", "v" }, "gk", function()
   -- `?` - Start a search backwards from the current cursor position.
   -- `^` - Match the beginning of a line.
   -- `##` - Match 2 ## symbols
@@ -635,7 +649,7 @@ end, { desc = "Go to previous markdown header" })
 -- Make sure to follow proper markdown convention, and you have a single H1
 -- heading at the very top of the file
 -- This will only search for H2 headings and above
-vim.keymap.set({ "n", "v" }, "gj", function()
+keymap.set({ "n", "v" }, "gj", function()
   -- `/` - Start a search forwards from the current cursor position.
   -- `^` - Match the beginning of a line.
   -- `##` - Match 2 ## symbols
@@ -649,7 +663,7 @@ vim.keymap.set({ "n", "v" }, "gj", function()
 end, { desc = "Go to next markdown header" })
 
 -- Surround the http:// url that the cursor is currently in with ``
-vim.keymap.set("n", "gsu", function()
+keymap.set("n", "gsu", function()
   local line = vim.api.nvim_get_current_line()
   local col = vim.api.nvim_win_get_cursor(0)[2] + 1 -- Adjust for 0-index in Lua
   -- This makes the `s` optional so it matches both http and https
@@ -682,7 +696,7 @@ end, { desc = "Add surrounding to URL" })
 -- ############################################################################
 
 -- Open image under cursor in the Preview app (macOS)
-vim.keymap.set("n", "<leader>io", function()
+keymap.set("n", "<leader>io", function()
   local function get_image_path()
     -- Get the current line
     local line = vim.api.nvim_get_current_line()
@@ -730,7 +744,7 @@ end, { desc = "[P](macOS) Open image under cursor in Preview" })
 -- BUT INSTEAD YOURE USING RELATIVE PATHS
 --
 -- If using absolute paths, use the default `gx` to open the image instead
-vim.keymap.set("n", "<leader>if", function()
+keymap.set("n", "<leader>if", function()
   local function get_image_path()
     -- Get the current line
     local line = vim.api.nvim_get_current_line()
